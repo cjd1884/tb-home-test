572d1a74386adcc5c90dbc5b3e9ed1f527bb22f6
Konstantinos Dimitros <k.dimitros@iit.demokritos.gr>
2016-08-24 10:05:52 +0300

    Home test project created

---
+++ TaxibeatHomeTest/TaxibeatHomeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxibeatHomeTest
+//
+//  Created by Konstantinos Dimitros on 24/08/16.
+//  Copyright © 2016 taxibeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaxibeatHomeTest/TaxibeatHomeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaxibeatHomeTest
+//
+//  Created by Konstantinos Dimitros on 24/08/16.
+//  Copyright © 2016 taxibeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
